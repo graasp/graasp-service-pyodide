35216b7554f649c02f2d1031b78d4fec54d76eab
Yves Piguet <yves.piguet@epfl.ch>
2020-01-23 15:30:40 +0100

    Debugger improvements

--- dbg.py
+++ dbg.py
@@ -16,10 +16,15 @@ def fact(n):
         print("n is 0")
         return 1
 
-from dbg import Dbg
-d = Dbg()
-d.set_breakpoint(4)
-d.run(fact, 10)
+from dbg import runcall
+runcall(fact, 10)
+
+or
+
+from dbg import runcode
+runcode('''c = fact(8) // (fact(3) * fact(8 - 3))
+print(c)
+''', locals=locals())
 
 """
 
@@ -27,6 +32,7 @@ import sys
 import os
 import traceback
 import textwrap
+import re
 
 class Dbg:
 
@@ -59,6 +65,9 @@ class Dbg:
     def set_breakpoint(self, lineno):
         self.breakpoints.add(lineno)
 
+    def clear_breakspoint(self, lineno):
+        self.breakpoints.remove(lineno)
+
     def run(self, fun, *args, **kwargs):
         self.fun = fun
         self.args = args
@@ -69,6 +78,9 @@ class Dbg:
         if self.interactive:
             self.cli()
 
+    def run_code(self, code, locals=None):
+        self.run(lambda: exec(code, globals(), locals))
+
     def resume(self, cmd):
         self.break_action.append((cmd,))
 
@@ -77,7 +89,7 @@ class Dbg:
             action_count += 1
         break_next = False
         frame_next = None
-        
+
         def trace(frame, event, arg):
             nonlocal action_count, break_next, frame_next
             action = self.break_action[action_count - 1][0]
@@ -106,52 +118,91 @@ class Dbg:
             suspended = True
         finally:
             sys.settrace(None)
+            self.enable_print(True)
         if not suspended:
             self.frame = None
 
+    def exec_cmd(self, cmd):
+        cmd = cmd.strip() or self.last_command
+        r = re.compile(r"^(\S+)(\s+(\S+))$")
+        match = r.match(cmd)
+        if match:
+            cmd0 = match.groups()[0]
+            arg = match.groups()[2]
+        else:
+            cmd0 = cmd
+            arg = None
+        if cmd0 in ["b", "break"]:
+            try:
+                lineno = int(arg)
+                self.set_breakpoint(lineno)
+            except:
+                print("Bad line number")
+            self.last_command = ""
+        elif cmd in ["c", "continue"]:
+            self.resume("c")
+            self.last_command = cmd
+        elif cmd0 in ["cl", "clear"]:
+            if arg:
+                try:
+                    lineno = int(arg)
+                    self.clear_breakspoint(lineno)
+                except:
+                    print("Bad line number")
+            else:
+                self.clear_breakspoints()
+            self.last_command = ""
+        elif cmd in ["h", "help"]:
+            print(textwrap.dedent("""
+                b(reak) line  break at specified line
+                c(ontinue)    continue
+                cl(ear)       clear all breakpoints
+                h(elp)        help
+                q(quit)       quit
+                r(eturn)      continue until the current function returns
+                s(tep)        step
+                w(here)       display a stack trace
+                expr          evaluate expression, including local and global variables
+                """))
+            self.last_command = ""
+        elif cmd in ["q", "quit"]:
+            self.frame = None
+        elif cmd in ["n", "next"]:
+            self.resume("n")
+            self.last_command = cmd
+        elif cmd in ["r", "return"]:
+            self.resume("r")
+            self.last_command = cmd
+        elif cmd in ["s", "step"]:
+            self.resume("s")
+            self.last_command = cmd
+        elif cmd in ["w", "where"]:
+            frame = self.frame
+            while frame and (frame.f_code.co_name != "resume"):
+                print(f"{frame.f_code.co_name} at line {frame.f_lineno}")
+                frame = frame.f_back
+        elif cmd != "":
+            try:
+                exec(compile(cmd, "<stdin>", "single"),
+                        self.frame.f_globals,
+                        self.frame.f_locals)
+            except Exception as e:
+                traceback.print_exception(etype=type(e),
+                                            value=e,
+                                            tb=e.__traceback__)
+
+        # return true when execution is completed
+        return self.frame is None
+
     def cli(self):
         while self.frame:
-            cmd = input(f"{self.frame.f_code.co_name}:{self.frame.f_lineno} dbg> ").strip() or self.last_command
-            if cmd in ["c", "continue"]:
-                self.resume("c")
-                self.last_command = cmd
-            elif cmd in ["cl", "clear"]:
-                self.clear_breakspoints()
-                self.last_command = ""
-            elif cmd in ["h", "help"]:
-                print(textwrap.dedent("""
-                    c(ontinue)  continue
-                    cl(ear)     clear all breakpoints
-                    h(elp)      help
-                    q(quit)     quit
-                    r(eturn)    continue until the current function returns
-                    s(tep)      step
-                    w(here)     display a stack trace
-                    expr        evaluate expression, including local and global variables
-                    """))
-                self.last_command = ""
-            elif cmd in ["q", "quit"]:
-                self.frame = None
-            elif cmd in ["n", "next"]:
-                self.resume("n")
-                self.last_command = cmd
-            elif cmd in ["r", "return"]:
-                self.resume("r")
-                self.last_command = cmd
-            elif cmd in ["s", "step"]:
-                self.resume("s")
-                self.last_command = cmd
-            elif cmd in ["w", "where"]:
-                frame = self.frame
-                while frame and (frame.f_code.co_name != "resume"):
-                    print(f"{frame.f_code.co_name} at line {frame.f_lineno}")
-                    frame = frame.f_back
-            elif cmd != "":
-                try:
-                    exec(compile(cmd, "<stdin>", "single"),
-                         self.frame.f_globals,
-                         self.frame.f_locals)
-                except Exception as e:
-                    traceback.print_exception(etype=type(e),
-                                              value=e,
-                                              tb=e.__traceback__)
+            cmd = input(f"{self.frame.f_code.co_name}:{self.frame.f_lineno} dbg> ")
+            self.exec_cmd(cmd)
+
+def runcall(fun, *args, **kwargs):
+    dbg = Dbg()
+    dbg.run(fun, *args, **kwargs)
+
+def runcode(code, locals=None):
+    dbg = Dbg()
+    dbg.run_code(code, locals)
