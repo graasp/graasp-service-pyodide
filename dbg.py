5d6fa84b6132d287ba6cd69443d8c63c7fcfa6b9
Yves Piguet <yves.piguet@epfl.ch>
2020-02-13 15:50:41 +0100

    In debugger, bug fixes related to next and return

--- dbg.py
+++ dbg.py
@@ -1,5 +1,5 @@
 # Simple debugger for Pyodide
-# Yves Piguet, EPFL, Jan 2020
+# Yves Piguet, EPFL, Jan-Feb 2020
 
 """
 Mechanism: run until the n:th breakpoint is reached, incrementing n
@@ -11,30 +11,30 @@ def fact(n):
     print("fact", n)
     if n > 0:
         print(n, "is strictly positive")
-        return n * fact(n - 1)
+        f1 = fact(n - 1)
+        return n * f1
     else:
         print("n is 0")
         return 1
 
-from dbg import runcall
-runcall(fact, 10)
+from dbg import debugcall
+debugcall(fact, 10)
 
 or
 
-from dbg import runcode
-runcode('''c = fact(8) // (fact(3) * fact(8 - 3))
+from dbg import debugcode
+code = '''c = fact(8) // (fact(3) * fact(8 - 3))
 print(c)
-''', locals=locals())
+'''
+debugcode(code, locals=locals(), breakpoints=[2])
 
 """
 
-import sys
-import os
-import traceback
-import textwrap
-import re
-
 class Dbg:
+
+    import sys
+    import os
+    import re
 
     class Suspended(Exception):
         pass
@@ -45,19 +45,26 @@ class Dbg:
 
         # breakpoint line numbers
         self.breakpoints = set()
+        self.break_at_start = True
         # for past breakpoints, tuple with action
-        # ("R", "c", "n", "s", or "r") and locals
+        # ("c"=continue, "n"=next, "s"=step, or "r"=return) and locals
+        # resume will execute them and suspend execution when they're exhausted
         self.break_action = []
+        # True to ignore trace calls until 2nd event="call"
+        self.ignore_top_call = False
 
         self.last_command = ""
 
-        self.stdout = sys.stdout
-        self.stderr = sys.stderr
-        self.null = open(os.devnull, "w")
+        self.init_output()
+        self.null = open(self.os.devnull, "w")
+
+    def init_output(self):
+        self.stdout = self.sys.stdout
+        self.stderr = self.sys.stderr
 
     def enable_print(self, on):
-        sys.stdout = self.stdout if on else self.null
-        sys.stderr = self.stderr if on else self.null
+        self.sys.stdout = self.stdout if on else self.null
+        self.sys.stderr = self.stderr if on else self.null
 
     def clear_breakspoints(self):
         self.breakpoints = set()
@@ -68,63 +75,90 @@ class Dbg:
     def clear_breakspoint(self, lineno):
         self.breakpoints.remove(lineno)
 
-    def run(self, fun, *args, **kwargs):
+    def is_suspended(self):
+        return self.frame is not None
+
+    def debug_(self, fun, ignore_top_call, args, kwargs):
         self.fun = fun
         self.args = args
         self.kwargs = kwargs
+        self.ignore_top_call = ignore_top_call
         self.returned_value = None
         self.break_action = []
-        self.resume("R")
+        self.resume(None)
         if self.interactive:
             self.cli()
 
-    def run_code(self, code, locals=None):
-        self.run(lambda: exec(code, globals(), locals))
+    def debug_call(self, fun, *args, **kwargs):
+        self.debug_(fun, False, args, kwargs)
+
+    def debug_code(self, code, locals=None):
+        self.debug_(lambda: exec(code, globals(), locals), True, (), {})
 
     def resume(self, cmd):
-        self.break_action.append((cmd,))
+        if cmd is not None:
+            self.break_action.append((cmd,))
 
+        # current state ("s"=step, "n"=next, "r"=return, "c"=continue)
+        state = "s" if self.break_at_start else "c"
+        # index of first action in self.break_action to perform
         action_count = 0
-        while action_count < len(self.break_action) and self.break_action[action_count][0] == "r":
-            action_count += 1
-        break_next = False
-        frame_next = None
+        # call depth used for "n" and "r"
+        call_depth = 0
+        last_break_depth = 0
+        # number of event "call"
+        call_count = 0
 
         def trace(frame, event, arg):
-            nonlocal action_count, break_next, frame_next
-            action = self.break_action[action_count - 1][0]
+            nonlocal state, action_count, call_depth, last_break_depth, call_count
+            if self.ignore_top_call:
+                if event == "call":
+                    call_count += 1
+                    call_depth += 1
+                if call_count < 2:
+                    return
             if event is "line":
-                if (action in ["R", "s"] or
-                    action == "n" and (frame is frame_next or break_next) or
-                    action == "r" and break_next or
+                self.current_line = line
+                if (state == "n" and call_depth <= last_break_depth or
+                    state == "r" and call_depth < last_break_depth or
+                    state == "s" or
                     frame.f_lineno in self.breakpoints):
+                    # breakpoint (explicit, or following s/n/r which should break here)
                     if action_count >= len(self.break_action):
+                        # not reached yet: really break here
                         self.frame = frame
                         raise self.Suspended()
                     else:
+                        # already reached: continue
+                        state = self.break_action[action_count][0]
                         action_count += 1
-                        frame_next = self.frame
+                        last_break_depth = call_depth
                         if action_count >= len(self.break_action):
                             self.enable_print(True)
+                elif action_count >= len(self.break_action):
+                    self.enable_print(True)
+            elif event is "call":
+                call_depth += 1
             elif event is "return":
-                break_next = break_next or action in ["n", "r"] and frame is frame_next
+                call_depth -= 1
             return trace
+
         suspended = False
         self.enable_print(len(self.break_action) <= 1)
-        sys.settrace(trace)
+        self.sys.settrace(trace)
         try:
             self.returned_value = self.fun(*self.args, **self.kwargs)
         except self.Suspended:
             suspended = True
         finally:
-            sys.settrace(None)
+            self.sys.settrace(None)
             self.enable_print(True)
         if not suspended:
             self.frame = None
 
     def exec_cmd(self, cmd):
         cmd = cmd.strip() or self.last_command
-        r = re.compile(r"^(\S+)(\s+(\S+))$")
+        r = self.re.compile(r"^(\S+)(\s+(\S+))$")
         match = r.match(cmd)
         if match:
             cmd0 = match.groups()[0]
@@ -133,11 +167,14 @@ class Dbg:
             cmd0 = cmd
             arg = None
         if cmd0 in ["b", "break"]:
-            try:
-                lineno = int(arg)
-                self.set_breakpoint(lineno)
-            except:
-                print("Bad line number")
+            if arg is None:
+                print(self.breakpoints)
+            else:
+                try:
+                    lineno = int(arg)
+                    self.set_breakpoint(lineno)
+                except:
+                    print("Bad line number")
             self.last_command = ""
         elif cmd in ["c", "continue"]:
             self.resume("c")
@@ -153,6 +190,7 @@ class Dbg:
                 self.clear_breakspoints()
             self.last_command = ""
         elif cmd in ["h", "help"]:
+            import textwrap
             print(textwrap.dedent("""
                 b(reak) line  break at specified line
                 c(ontinue)    continue
@@ -187,22 +225,26 @@ class Dbg:
                         self.frame.f_globals,
                         self.frame.f_locals)
             except Exception as e:
+                import traceback
                 traceback.print_exception(etype=type(e),
-                                            value=e,
-                                            tb=e.__traceback__)
+                                          value=e,
+                                          tb=e.__traceback__)
 
         # return true when execution is completed
         return self.frame is None
 
     def cli(self):
-        while self.frame:
+        while self.is_suspended():
             cmd = input(f"{self.frame.f_code.co_name}:{self.frame.f_lineno} dbg> ")
             self.exec_cmd(cmd)
 
-def runcall(fun, *args, **kwargs):
+def debugcall(fun, *args, **kwargs):
     dbg = Dbg()
-    dbg.run(fun, *args, **kwargs)
+    dbg.debug_call(fun, *args, **kwargs)
 
-def runcode(code, locals=None):
+def debugcode(code, locals=None, breakpoints=None):
     dbg = Dbg()
-    dbg.run_code(code, locals)
+    if breakpoints:
+        for breakpoint in breakpoints:
+            dbg.set_breakpoint(breakpoint)
+    dbg.debug_code(code, locals)
